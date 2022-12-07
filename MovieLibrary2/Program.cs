25f2a687d4c0e3a0d53e5f7fb06875b234c1cf5a
Andy Berthelsen <aberthelsen@my.wctc.edu>
2022-12-07 05:31:45 -0600

    Update with Add User and User Search

--- MovieLibrary2/Program.cs
+++ MovieLibrary2/Program.cs
@@ -1,15 +1,24 @@
 ﻿using MovieLibraryEntities.Context;
 using MovieLibraryEntities.Models;
+using Microsoft.EntityFrameworkCore;
 using System.Runtime.InteropServices;
 using System.Transactions;
+using System.Linq;
+using MovieLibrary2;
 
 string input;
+
+
 do
 {
     Console.WriteLine("1. Search Movie");
     Console.WriteLine("2. Add Movie");
     Console.WriteLine("3. Update Movie");
     Console.WriteLine("4. Delete Movie");
+    Console.WriteLine("5. List all movies");
+    Console.WriteLine("6. Search by year");
+    Console.WriteLine("7. User Searches");
+    Console.WriteLine("8. Add a User");
     Console.WriteLine("0. Exit");
     input = Console.ReadLine();
 
@@ -24,12 +33,19 @@ do
             {
                 using (var db = new MovieContext())
                                     {
-                    var listOfMovies = db.Movies.ToList();
-                   var searchResults = listOfMovies.Where(m => m.Title.Contains(movieSearch, StringComparison.OrdinalIgnoreCase));
+                    var searchResults = db.Movies.Include(m => m.MovieGenres).ThenInclude(m => m.Genre).Where(m => m.Title.Contains(movieSearch));
+                  // var searchResults = listOfMovies.Where(m => m.Title.Contains(movieSearch, StringComparison.OrdinalIgnoreCase));
                     var count = 0;
                     foreach (var movie in searchResults)
                     {       
                         Console.WriteLine($"{movie.Id}) {movie.Title} {movie.ReleaseDate}");
+
+                        Console.Write("Genre(s): ");
+                        foreach (var g in movie.MovieGenres)
+                            {
+                            Console.Write($" {g.Genre.Name} "); 
+                        }
+                        Console.WriteLine();
                         count++;
                     }
                     if (count == 1)
@@ -148,7 +164,119 @@ do
                 }
             }
             break;
+
+        case "5":
+            {
+                using (var db = new MovieContext())
+                {
+                    var listOfMovies = db.Movies;
+                    foreach (var movie in listOfMovies)
+                    {
+                        Console.WriteLine($"{movie.Id}) {movie.Title} {movie.ReleaseDate}");
+                        
+                    }
+                } 
             }
+            break;
+
+        case "6":
+            // search for movie
+            Console.WriteLine("Enter year");
+            movieSearch = Console.ReadLine();
+
+            try
+            {
+                using (var db = new MovieContext())
+                {
+                    var listOfMovies = db.Movies.ToList();
+                    var searchResults = listOfMovies.Where(m => m.ReleaseDate.ToString().Contains(movieSearch));
+                    var count = 0;
+                    foreach (var movie in searchResults)
+                    {
+                        Console.WriteLine($"{movie.Id}) {movie.Title} {movie.ReleaseDate}");
+                        count++;
+                    }
+                    if (count == 1)
+                        Console.WriteLine($"{count} movie from {movieSearch} was found");
+
+                    else
+                        Console.WriteLine($"{count} movies from {movieSearch} were found");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("There was an error with your search!");
+            }
+            break;
+
+      
+
+        case "7":
+            {
+                var userSearch = new UserSearch();
+            }
+            break;
+
+        case "8":
+            {
+                Console.WriteLine("Add a new user \n");
+                Console.WriteLine("Enter user's age");
+                var age = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter user's gender");
+                var gender = Convert.ToString(Console.ReadLine()[0]).ToUpper();
+                Console.WriteLine("Enter user's zip code");
+                var zip = Console.ReadLine();
+                Console.WriteLine("Enter user's occupation");
+                var occ = Console.ReadLine();
+
+               using (var db = new MovieContext())
+
+                                  
+
+                    try
+                    {
+                        if (db.Occupations.FirstOrDefault(x => x.Name == occ) == null)
+                        {
+                            var newOcc = new Occupation()
+                            {
+                                Name = occ
+                            };
+                            db.Occupations.Add(newOcc);
+                            db.SaveChanges();
+                        }
+
+                       
+                        var occID = db.Occupations.FirstOrDefault(x => x.Name == occ);
+
+                        var newUser = new User()
+                        {
+                            Age = age,
+                            Gender = gender,
+                            ZipCode = zip,
+                            Occupation = occID
+
+                        };
+                        db.Users.Add(newUser);
+                        db.SaveChanges();
+
+                        var newEntry = db.Users.AsEnumerable().Last();
+                        Console.WriteLine($"{newEntry.Id} {newEntry.Age} {newEntry.Gender} {newEntry.ZipCode} {newEntry.Occupation.Name}");
+
+                    }
+                    catch (Exception)
+                    {
+
+                        Console.WriteLine("There was an error"); ;
+                    }
+                    
+
+                        
+                     
+                }
+            break;
+    }
+            
+    
             
     
 
---
+++ MovieLibrary2/UserSearch.cs
@@ -0,0 +1,183 @@
+﻿using MovieLibraryEntities.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Transactions;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MovieLibrary2
+{
+    internal class UserSearch
+    {
+        public UserSearch() {
+            string input;
+            do 
+            {
+                Console.WriteLine("User Search");
+                Console.WriteLine("1. List All Users");
+                Console.WriteLine("2. List All Users by Occupation");
+                Console.WriteLine("3. Search Users by Occupation");
+                Console.WriteLine("4. Search Users by Zip Code");
+                Console.WriteLine("5. Movies Rating by User");
+                Console.WriteLine("0. Exit User Search");
+
+                input = Console.ReadLine();
+
+                try
+                {
+                    switch (input) {
+
+                        case "1":
+                            {
+                                using (var db = new MovieContext())
+                                {
+                                    var listOfUsers = db.Users.Include(x => x.Occupation).OrderBy(x => x.Id);
+                                    foreach (var user in listOfUsers)
+                                    {
+                                        Console.WriteLine($"{user.Id}) {user.Age} {user.Gender} {user.ZipCode} {user.Occupation.Name} ");
+
+                                    }
+                                }
+                            } break;
+
+                        case "2":
+                            {
+                                using (var db = new MovieContext())
+                                {
+                                    var listOfUsers = db.Users.Include(x => x.Occupation);
+                                    foreach (var user in listOfUsers)
+                                    {
+                                        Console.WriteLine($"{user.Id}) {user.Age} {user.Gender} {user.Occupation.Name}");
+
+                                    }
+                                }
+                            } break;
+
+                            case "3":
+                            {
+                                Console.WriteLine("What Occupation would you like search for?");
+                                var occSearch = Console.ReadLine();
+
+                                try
+                                {
+                                    using (var db = new MovieContext())
+                                    {
+                                        var listOfUsers = db.Users.Where(x => x.Occupation.Name == occSearch);
+                                        var count = 0;
+
+                                        foreach (var user in listOfUsers)
+                                        {
+                                            Console.WriteLine($"{user.Id}) {user.Age} {user.Gender} {user.ZipCode}");
+                                            count++;
+                                        }
+
+                                        if (count != 1)
+                                        Console.WriteLine($"There were {count} users listed under {occSearch}");
+                                        else
+                                            Console.WriteLine($"There was {count} user listed under {occSearch}");
+                                    }
+                                }
+                                catch (Exception)
+                                {
+
+                                    Console.WriteLine("There was an error");
+                                }
+                                
+
+                            } break;
+
+                            case "4":
+                            {
+                                Console.WriteLine("Enter the Zip Code you want to search for");
+                                var zipSearch = Console.ReadLine();
+
+                                try
+                                {
+                                    using (var db = new MovieContext())
+                                    {
+                                        var listOfUsers = db.Users.Include(x => x.Occupation).Where(x => x.ZipCode == zipSearch);
+                                        var count = 0;
+
+                                        foreach (var user in listOfUsers)
+                                        {
+                                            Console.WriteLine($"{user.Id}) {user.Age} {user.Gender} {user.Occupation.Name}");
+                                            count++;
+                                        }
+
+                                        if (count != 1)
+                                            Console.WriteLine($"There were {count} users listed the Zip Code {zipSearch}");
+                                        else
+                                            Console.WriteLine($"There was {count} user with the Zip Code {zipSearch}");
+                                    }
+                                }
+                                catch (Exception)
+                                {
+
+                                    Console.WriteLine("There was an error");
+                                }
+
+
+                            }break;
+
+                            case "5":
+                            {
+                                Console.WriteLine("Enter the User Id for User you want to search for");
+                                var userIDSearch = Convert.ToInt32( Console.ReadLine() );
+
+                                try
+                                {
+                                    using (var db = new MovieContext())
+                                    {
+                                        var listOfMovies = db.UserMovies.Include(x => x.Movie).Where(x => x.User.Id == userIDSearch);
+                                        var count =0;
+                                        long total = 0;
+                                        
+                                            foreach (var movie in listOfMovies)
+                                            {
+                                                Console.WriteLine($"Title: {movie.Movie.Title} Rating: {movie.Rating}");
+                                            count++;
+                                             total += movie.Rating;
+                                            
+                                            }
+                                        if (count <= 0)
+                                            Console.WriteLine($"User {userIDSearch} hasn't rated any movies");
+                                        else if (count == 1)
+                                        {
+                                            Console.WriteLine($"User {userIDSearch} has rated {count} with a ranking of {total}");
+                                        }
+                                       
+                                        else if (count > 1)
+                                        {
+                                            Console.WriteLine($"User {userIDSearch} has rated {count} movies with an average rating of {total/count}");
+                                        }
+                                        
+                                        
+                                        
+                                    }
+
+                                }
+                                catch (Exception)
+                                {
+
+                                    throw;
+                                }
+
+                            }break;
+
+
+
+                    }
+                }
+            catch (Exception)
+                {
+
+                    Console.WriteLine("There is an error");
+                }
+            } while (input != "0"); 
+            
+        }
+    }
+}
